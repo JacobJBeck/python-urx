1def625524e0310ec7b15f79c67ab06e35aeaad6
Adam Allevato <adam.d.allevato@gmail.com>
2017-06-09 09:21:57 -0700

    ROS 2-ified

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
-from distutils.core import setup
-from distutils.command.install_data import install_data
+from setuptools import setup
 
-
-setup (name = "urx", 
-        version = "0.10.0",
-        description = "Python library to control an UR robot",
-        author = "Olivier Roulet-Dubonnet",
-        author_email = "olivier.roulet@gmail.com",
-        url = 'https://github.com/oroulet/python-urx',
-        packages = ["urx"],
-        provides = ["urx"],
-        install_requires=["math3d"],
-        license = "GNU Lesser General Public License v3",
-
-        classifiers = [
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 3",
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Developers",
-            "Operating System :: OS Independent",
-            "Topic :: System :: Hardware :: Hardware Drivers",
-            "Topic :: Software Development :: Libraries :: Python Modules",
-        ]
-        )
-
-
+setup(
+    name='python_urx',
+    version='0.10.0',
+    packages=["urx"],
+    py_modules=[],
+    install_requires=['setuptools'],
+    author = "Olivier Roulet-Dubonnet",
+    author_email = "olivier.roulet@gmail.com",
+    maintainer='Adam Allevato',
+    maintainer_email='allevato@osrfoundation.org',
+    keywords=['ROS'],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Hardware :: Hardware Drivers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    description=(
+        "Python library to control an UR robot"
+    ),
+    license='GNU Lesser General Public License v3',
+    entry_points={},
+)
